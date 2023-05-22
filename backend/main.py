1eaf7f8d33754207bda6589ea84ecebb91fc7d0a
kingAuthor1212 <kingauthor1212@gmail.com>
2023-05-22 10:32:59 +0200

    react-python-app

---
+++ backend/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from pydantic import BaseModel
+
+import requests
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+PRIVATE_KEY = "12149014-db45-48d8-bda9-04806a015c35"
+
+class User(BaseModel):
+    username: str
+
+@app.post('/authenticate')
+async def authenticate(user: User):
+    response = requests.put('https://api.chatengine.io/users/',
+        data={
+            "username": user.username,
+            "secret": user.username,
+            "first_name": user.username,
+        },
+        headers={ "Project-ID": PRIVATE_KEY }
+    )
+    return response.json()
